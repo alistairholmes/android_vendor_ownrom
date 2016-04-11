ac242d64bfc8db218ae7bd93dbec4afad120459c
Akhil Narang <akhilnarang.1999@gmail.com>
2016-04-11 16:43:31 +0530

    vendor: import scripts from OwnROM Lollipop
     * Also update sync script a bit, since Ik more about bash now :P
     * The sync script will delete the repositories and sync them!
     * Be sure to not keep any unsaved work, else only a reflog can save you.
    
    Change-Id: Ic5d4f4cf4c451b478cf5b8a45aa4e53f5f294408
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ tools/sync-own.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright � 2015-2016, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>, Team OwnROM
+# Custom Sync Script for OwnROM repositories
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+#
+
+if [ -d ".repo" ]
+then
+. build/envsetup.sh > /dev/null
+dir=${PWD}
+tools=${dir}/vendor/ownrom/tools
+. $tools/colors
+echo -e "$bldred Syncing OwnROM repositories......"
+$normal
+for repository in $(cat $tools/ownrom-repos)
+do
+rm -rf ${repository}
+echo -e "$bldblu Syncing $repository"
+echo -e "$bldcya `repo sync -c -q -f --force-sync --no-clone-bundle --no-tags $repository`"
+echo -e "$bldgrn Synced $repository"
+echo -e ""
+done
+$normal
+else
+echo -e "Please make sure you are running this in the root of OwnROM Source!
+exit 1;
+fi
+
