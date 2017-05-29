15dca0c767dd9c4d0f3585829529f32a7e246b27
OwnDroid <owndroid26@gmail.com>
2017-05-29 09:28:53 +0200

    Vendor: Bring in NovaLauncher
    Signed-off-by: OwnDroid <owndroid26@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -38,6 +38,7 @@ endif
 # Custom Prebuilt OwnROM Packages
 PRODUCT_COPY_FILES += \
 vendor/ownrom/prebuilt/apps/Amaze.apk:system/priv-app/Amaze/Amaze.apk
+vendor/ownrom/prebuilt/apps/Nova.apk:system/priv-app/Nova/Nova.apk
 vendor/ownrom/prebuilt/apps/Substratum.apk:sytem/priv-app/Substratum/Substratum.apk
 
 # Backup Tool
@@ -130,7 +131,6 @@ PRODUCT_PACKAGES += \
     LockClock \
     OwnOTA \
     ThemeInterfacer \
-    Trebuchet \
     WallpaperPicker \
     WeatherProvider
 
